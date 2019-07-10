13b186abd4cbec3432330328d64beda4ad3ef039
Bartek Karelus <bartek.karelus@gmail.com>
2019-07-10 16:40:49 +0200

    movies.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Movies from "./Movies"
+
 
 class App extends Component {
+
+  state = {
+    movies: [],
+    search: ""
+  }
+
+  handleChange = (e) => {
+    const value = e.target.value
+    const ApiKey = "5259c8949c37b92e4bfb71d3a1948220"
+    console.log(value)
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${ApiKey}&query=${value}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.results)
+
+        this.setState({
+          movies: data.results
+        })
+      })
+  }
+
   render() {
+    const movies = this.state.movies
+
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <>
+        <div>
+          MOVIE BROWSER
+        </div>
+        <div>
+          <label htmlFor="search">Wyszukaj film</label>
+          <input onChange={this.handleChange} type="text" placeholder="Wpisz tytuł filmu" id="search"></input>
+        </div>
+        <h4>
+          <Movies results={movies} />
+        </h4>
+      </>
+    )
+
+
+
   }
 }
-
 export default App;
---
+++ src/Movies.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+
+
+const Movies = (props) => {
+    const movies = props.results
+
+    const results = movies.map(item => (
+
+        < div key={item.id} >
+            <h4>{item.title} </h4>
+
+            <img src={`https://image.tmdb.org/t/p/w185${item.poster_path}`} alt="poster of movie" />
+            <p>Ocena internautów: {item.vote_average}</p>
+
+        </div >
+    ))
+
+
+    return (
+        <div>
+            <ul>
+                {results}
+            </ul>
+        </div>
+    )
+
+}
+
+export default Movies
