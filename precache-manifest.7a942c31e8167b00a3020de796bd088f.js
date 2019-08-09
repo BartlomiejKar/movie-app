5eef90c4d48f13906348a64bbe5893907ded5443
Bartek Karelus <bartek.karelus@gmail.com>
2019-08-09 19:13:29 +0200

    Updates

---
+++ precache-manifest.7a942c31e8167b00a3020de796bd088f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c25a838aa58df060864f44400b945b3b",
+    "url": "/static/media/filmy.c25a838a.jpeg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4c77a3f2811960685620",
+    "url": "/static/js/main.467de67d.chunk.js"
+  },
+  {
+    "revision": "22b162a17c2685a38d97",
+    "url": "/static/js/2.100e50e4.chunk.js"
+  },
+  {
+    "revision": "4c77a3f2811960685620",
+    "url": "/static/css/main.1a5db1d1.chunk.css"
+  },
+  {
+    "revision": "daa63e1fc0a1ec7483cdb7bdf98161f7",
+    "url": "/index.html"
+  }
+];
