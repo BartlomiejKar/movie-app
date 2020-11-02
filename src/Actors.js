d5d5dce9c9ce6224631110f8e05f9374d1886241
Bartek Karelus <bartek.karelus@gmail.com>
2020-11-02 20:29:46 +0100

    fixed error in console. clean UseEffect. used async await function  in useEffect

--- src/Actors.js
+++ src/Actors.js
@@ -6,11 +6,16 @@ const Actors = ({ ApiKey }) => {
     const [actors, setActors] = useState([]);
     const [value, setValue] = useState("")
 
-    useEffect(async () => {
-        const response = await fetch(`https://api.themoviedb.org/3/search/person?api_key=${ApiKey}&language=en-US&query=jason&page=1&include_adult=false`);
-        const data = await response.json()
-        const results = data.results
-        setActors(results)
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(`https://api.themoviedb.org/3/search/person?api_key=${ApiKey}&language=en-US&query=jason&page=1&include_adult=false`);
+            const data = await response.json()
+            const results = data.results
+            const knownFor = data.results[0].known_for[0].title
+            console.log(knownFor)
+            setActors(results)
+        }
+        fetchData()
     }, [])
     console.log(actors)
     const ActorsArray = actors.map(el => {
@@ -18,6 +23,7 @@ const Actors = ({ ApiKey }) => {
             <div>
                 <p>{el.name}</p>
                 <p>{el.popularity}</p>
+                <p>{el.known_for[0].title}</p>
             </div>
         )
     })
