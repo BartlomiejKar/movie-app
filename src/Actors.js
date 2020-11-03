c3d3b19961f71d11451de9052793626e7ad30d92
Bartek Karelus <bartek.karelus@gmail.com>
2020-11-03 18:45:00 +0100

    added img and noPhoto. added input to search actors

--- src/Actors.js
+++ src/Actors.js
@@ -1,39 +1,51 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
+
+import "./css/Actors.css"
+import noPhoto from "./noPhoto.jpg"
 
 
 const Actors = ({ ApiKey }) => {
     const [actors, setActors] = useState([]);
-    // const [value, setValue] = useState("")
+    const [value, setValue] = useState("")
 
-    useEffect(() => {
+
+    const handleSearchActor = (e) => {
+        const value = e.target.value
+        setValue(value)
         const fetchData = async () => {
-            const response = await fetch(`https://api.themoviedb.org/3/search/person?api_key=${ApiKey}&language=en-US&query=jason Star&page=1&include_adult=false`);
+            const response = await fetch(`https://api.themoviedb.org/3/search/person?api_key=${ApiKey}&language=en-US&query=${value}&page=1&include_adult=false`);
             const data = await response.json()
             const results = data.results
-            console.log(results)
             setActors(results)
         }
         fetchData()
-    }, [])
-    const ActorsArray = actors.map(({ id, name, popularity, known_for }) => {
+    }
 
-        const knowFromMovies = known_for.map(el => {
-            return el.title
+
+
+    const ActorsArray = actors ? actors.map(({ id, name, popularity, known_for = [], profile_path }) => {
+
+        const knowFromMovies = known_for.map((el, index) => {
+            return (
+                <li key={index}>{el.title}</li>
+            )
         })
         return (
-            <div key={id}>
+            <div className="actor_container" key={id}>
                 <p>{name}</p>
                 <p>{popularity}</p>
-                <p>{knowFromMovies}</p>
+                <ul>{knowFromMovies}</ul>
+                {profile_path ? < img className="actor_img" alt="aktor" src={`https://image.tmdb.org/t/p/w185${profile_path}`} /> : <img src={noPhoto} alt="empty" />}
             </div>
         )
-    })
+    }) : null
 
 
     return (
         <>
             < p >Aktorzy</p >
+            <input placeholder="wyszukaj aktora" value={value} onChange={handleSearchActor} type="text" />
             {ActorsArray}
         </>
 
