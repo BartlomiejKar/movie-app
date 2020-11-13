2bceca3d70a4c5d46ebe13bad95ed185c3124956
Bartek Karelus <bartek.karelus@gmail.com>
2020-11-13 21:50:39 +0100

    added footer style, links,

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,16 +1,20 @@
 import React from 'react';
 
 import "../css/Footer.css"
+import github from "../img/GitHub-Mark-Light-64px.png"
+
+
 
 const Footer = () => {
     return (
         <footer>
-            <ul>
-                <li>IMDB</li>
-                <li>Filmweb</li>
-                <li>The Movie DB</li>
+            <a className="footer_github" href="https://github.com/BartlomiejKar"> <img className="footer_github" src={github} alt="github" /></a>
+
+            <ul className="footer_list">
+                <li className="footer_element-li"><a href="https://www.imdb.com/" className="footer_link">IMDB</a></li>
+                <li className="footer_element-li"><a href="https://www.filmweb.pl/" className="footer_link">Filmweb</a></li>
+                <li className="footer_element-li"><a href="https://www.themoviedb.org" className="footer_link">The Movie DB</a> </li>
             </ul>
-            <p>2020</p>
         </footer>
     )
 }
